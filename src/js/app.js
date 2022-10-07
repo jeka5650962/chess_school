90204fbe7fbbff7abca4e7e99375c8df9de819ec
Evgeniy Salish <jeka5650962@gmail.com>
2022-10-07 13:42:53 +0300

    Почти закончил адаптив шапки

--- src/js/app.js
+++ src/js/app.js
@@ -1,7 +1,9 @@
 import * as myFunctions from "./modules/functions.js";
+import * as myBurger from "./modules/burger.js";
 
 myFunctions.isWebp();
+myBurger.onBurger();
 
-import Swiper, { Navigation, Pagination } from 'swiper';
+import Swiper, {Navigation, Pagination} from 'swiper';
 
 const swiper = new Swiper();
