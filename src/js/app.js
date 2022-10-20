3c262fc308c98db59a6cfbb0c2d90e8a0d2f422f
Evgeniy Salish <jeka5650962@gmail.com>
2022-10-20 13:17:55 +0300

    Начинаю делать slider 201022_13:17

--- src/js/app.js
+++ src/js/app.js
@@ -4,6 +4,15 @@ import * as myBurger from "./modules/burger.js";
 myFunctions.isWebp();
 myBurger.onBurger();
 
+// core version + navigation, pagination modules:
 import Swiper, {Navigation, Pagination} from 'swiper';
+// import Swiper and modules styles
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
 
-const swiper = new Swiper();
+// init Swiper:
+const swiper = new Swiper('.swiper', {
+// configure Swiper to use modules
+    modules: [Navigation, Pagination],
+});
