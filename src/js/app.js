9c3b600e6d9ed3b376139b983315964365864825
Evgeniy Salish <jeka5650962@gmail.com>
2022-10-20 16:33:04 +0300

    Продолжаю делать slider 201022_16:32

--- src/js/app.js
+++ src/js/app.js
@@ -4,9 +4,3 @@ import * as myBurger from "./modules/burger.js";
 myFunctions.isWebp();
 myBurger.onBurger();
 
-// import Swiper JS
-import Swiper from 'swiper';
-// import Swiper styles
-// import 'swiper/css';
-
-const swiper = new Swiper('.image-slider');
