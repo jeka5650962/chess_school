08517e94fb186d1a7d55759e4758a5137e43355b
Evgeniy Salish <jeka5650962@gmail.com>
2022-10-31 13:56:20 +0300

    Делаю faq 311022_13:56

--- src/js/app.js
+++ src/js/app.js
@@ -4,3 +4,10 @@ import * as myBurger from "./modules/burger.js";
 myFunctions.isWebp();
 myBurger.onBurger();
 
+const faqBlock = document.getElementsByClassName('faq__content');
+
+for (let i = 0; i < faqBlock.length; i++) {
+    faqBlock[i].addEventListener('click', function () {
+        this.classList.toggle('active')
+    })
+}
