7836b61b8182e255a618c38cdef806544f23ef1f
Evgeniy Salish <jeka5650962@gmail.com>
2022-11-01 12:27:40 +0300

    Продолжаю делать faq 011122_12:27

--- src/js/app.js
+++ src/js/app.js
@@ -4,6 +4,8 @@ import * as myBurger from "./modules/burger.js";
 myFunctions.isWebp();
 myBurger.onBurger();
 
+// ---------------------------- faqBlock и faqContent
+
 const faqBlock = document.getElementsByClassName('faq__question');
 
 for (let i = 0; i < faqBlock.length; i++) {
@@ -11,3 +13,12 @@ for (let i = 0; i < faqBlock.length; i++) {
         this.classList.toggle('active')
     })
 }
+
+const faqContent = document.getElementsByClassName('faq__content');
+
+for (let i = 0; i < faqContent.length; i++) {
+    faqContent[i].addEventListener('click', function () {
+        this.classList.toggle('opened')
+    })
+}
+
