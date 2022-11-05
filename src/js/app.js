efc843847613637f3dc3891ae9cbbeeb9565aa78
Evgeniy Salish <jeka5650962@gmail.com>
2022-11-05 12:19:32 +0300

    Делаю форму 051122_12:19

--- src/js/app.js
+++ src/js/app.js
@@ -25,6 +25,25 @@ document.addEventListener('DOMContentLoaded', function () {
 
         let error = formValidate(form)
 
+        let formData = new FormData(form)
+
+        if (error === 0) {
+            form.classList.add('_sending')
+            let response = await fetch('sendmail.php', {
+                method: 'POST',
+                body: formData
+            })
+            if (response.ok) {
+                let result = await response.json()
+                alert(result.message)
+                form.reset()
+            } else {
+                alert('Ошибка')
+            }
+        } else {
+            alert('Заполните обязательные поля')
+        }
+
         function formValidate(form) {
             let error = 0
             let formReq = document.querySelectorAll('._req')
@@ -45,6 +64,7 @@ document.addEventListener('DOMContentLoaded', function () {
                     }
                 }
             }
+            return error
         }
 
         function formAddError(input) {
