bf247980ec0349414be3aef6a4eacad7350e0b9b
Evgeniy Salish <jeka5650962@gmail.com>
2022-11-08 19:45:14 +0300

    Делаю popup 081122_19:45

--- src/js/app.js
+++ src/js/app.js
@@ -144,12 +144,14 @@ function popupClose(popupActive, doUnlock = true) {
     }
 }
 
-function bodyUnLock() {
+function bodyLock() {
     const lockPaddingValue = window.innerWidth - document.querySelector('.wrapper').offsetWidth + 'px'
 
-    for (let index = 0; index < lockPadding.length; index++) {
-        const el = lockPadding[index]
-        el.style.paddingRight = lockPaddingValue
+    if (lockPadding.length > 0) {
+        for (let index = 0; index < lockPadding.length; index++) {
+            const el = lockPadding[index]
+            el.style.paddingRight = lockPaddingValue
+        }
     }
     body.style.paddingRight = lockPaddingValue
     body.classList.add('lock')
@@ -159,3 +161,54 @@ function bodyUnLock() {
         unlock = true
     }, timeout)
 }
+
+function bodyUnLock() {
+    setTimeout(function () {
+        if (lockPadding.length > 0) {
+            for (let index = 0; index < lockPadding.length; index++) {
+                const el = lockPadding[index]
+                el.style.paddingRight = '0px'
+            }
+        }
+        body.style.paddingRight = '0px'
+        body.classList.remove('lock')
+    }, timeout)
+
+    unlock = false
+    setTimeout(function () {
+        unlock = true
+    }, timeout)
+}
+
+document.addEventListener('keydown', function (e) {
+    if (e.which === 27) {
+        const popupActive = document.querySelector('.popup.open')
+        popupClose(popupActive)
+    }
+})
+
+(function () {
+    // проверяем поддержку
+    if (!Element.prototype.closest) {
+        // реализуем
+        Element.prototype.closest = function (css) {
+            var node = this
+            while (node) {
+                if (node.matches(css)) return node
+                else node = node.parentElement
+            }
+            return null
+        }
+    }
+})()
+
+(function () {
+    // проверяем поддержку
+    if (!Element.prototype.matches) {
+        // определяем свойство
+        Element.prototype.matches = Element.prototype.matchesSelector ||
+            Element.prototype.webkitMatchesSelector ||
+            Element.prototype.mozMatchesSelector ||
+            Element.prototype.msMatchesSelector
+    }
+})()
