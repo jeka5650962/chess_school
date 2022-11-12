c61787988534e72525f87415e82e788b7d637592
Evgeniy Salish <jeka5650962@gmail.com>
2022-11-12 22:07:49 +0300

    Делаю стрелки для тачскринов в header menu 121122 22:07

--- src/js/modules/drop-menu.js
+++ src/js/modules/drop-menu.js
@@ -24,6 +24,17 @@ export function dropMenu() {
 
     if (isMobile.any()) {
         bodyElement.classList.add('touch')
+        let arrow = document.querySelectorAll('.arrow')
+        for (let i = 0; i < arrow.length; i++) {
+            let thisLink = arrow[i].previousElementSibling
+            let subMenu = arrow[i].nextElementSibling
+            let thisArrow = arrow[i]
+            thisLink.classList.add('parent')
+            arrow[i].addEventListener('click', function () {
+                subMenu.classList.toggle('open')
+                thisArrow.classList.toggle('active')
+            })
+        }
     } else {
         bodyElement.classList.add('mouse')
     }
